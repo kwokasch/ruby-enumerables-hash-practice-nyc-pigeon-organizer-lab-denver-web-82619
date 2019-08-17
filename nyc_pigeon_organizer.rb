5d1bb08aecce0a1662cf94789c8e8d9a7d3a8725
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-17 16:04:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,10 @@
 def nyc_pigeon_organizer(data)
   new_pigeon_list = {}
   
-  data.each do |descriptor, attribute_hash|
+  data.each do |descriptor, attribute_hash| 
+    #=> in the passed in hash, grab the keys ("descriptor") and the values (hashes) associated with those keys
     attribute_hash.each do |attribute, name_array|
+       #=> in the hashes associated with the "descriptor" keys, grab the keys ("descriptor") and the values (hashes) associated with those keys
       name_array.each do |name|
       
       new_pigeon_list["#{name}"] = {:color => [], :gender => [], :lives => []} 
