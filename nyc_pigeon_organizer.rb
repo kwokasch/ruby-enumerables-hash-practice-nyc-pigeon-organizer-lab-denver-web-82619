0fbaa1b51df9274abf15131cf30596f62c488b98
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-17 15:47:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,32 +5,37 @@ def nyc_pigeon_organizer(data)
   
   data.each do |descriptor, attribute_hash|
     attribute_hash.each do |attribute, name_array|
+      pigeon_name = attribute.values.first
       name_array.each do |name|
-      new_pigeon_list["#{name}"] = {:color => [], :gender => [], :lives => []} 
+      
+      new_pigeon_list["#{pigeon_name}"] = {:color => [], :gender => [], :lives => []} 
       end
     end
   end
      
   data[:color].each do |attribute, name_array|
     name_array.each do |name|
-      if data[:color][attribute].include?(name)
-        new_pigeon_list[name][:color] << attribute.to_s 
+      pigeon_name = attribute.values.first
+      if data[:color][attribute].include?(pigeon_name)
+        new_pigeon_list["#{pigeon_name}"][:color] << attribute.to_s 
       end
     end
   end
   
   data[:gender].each do |attribute, name_array|
     name_array.each do |name|
-      if data[:gender][attribute].include?(name)
-        new_pigeon_list[name][:gender] << attribute.to_s 
+      pigeon_name = attribute.values.first
+      if data[:gender][attribute].include?(pigeon_name)
+        new_pigeon_list["#{pigeon_name}"][:gender] << attribute.to_s 
       end
     end
   end
   
   data[:lives].each do |attribute, name_array|
     name_array.each do |name|
-      if data[:lives][attribute].include?(name)
-        new_pigeon_list[name][:lives] << attribute.to_s 
+      pigeon_name = attribute.values.first
+      if data[:lives][attribute].include?(pigeon_name)
+        new_pigeon_list["#{pigeon_name}"] << attribute.to_s 
       end
     end
   end
