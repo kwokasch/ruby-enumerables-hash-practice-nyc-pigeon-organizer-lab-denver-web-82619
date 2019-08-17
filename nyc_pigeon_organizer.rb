be6184a7b31ea9cf5e9d95750ef2a6a8c1028459
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-17 15:22:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,8 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   
-  data.each do |descriptor, attributes|
-    attributes.each do |specific_descriptor|
+  data.each do |descriptor, attribute_hash|
+    attribute_hash.each do |attribute, name_array|
       pigeon_name = specific_descriptor.values.first 
       
       pigeon_list["#{pigeon_name}"] = {:color => pigeon_data[:color] => specific_descriptor
